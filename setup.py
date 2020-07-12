5c1c905d15538722ba6380cf5a52f894509c0d47
Evan <evansmeath@gmail.com>
2020-07-12 18:23:15 +0100

    Changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='python-vectors-EvsDev',
-    version='1.0.0',
+    version='1.0.1',
     author='EvsDev',
     description="A simple vectors package',
     long_description=long_description,
